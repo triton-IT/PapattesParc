2c56cbf7545272ac981aea688dc12c08e6db87ad
Régis Ramillien <r.ramillien@tritonit.tech>
2026-07-31 20:41:25 +0200

    Context menu on right click in web distribution is now really ignored.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app/game_app.dart';
 import 'games/pattes_friandises/data/match3_progress_store.dart';
@@ -7,6 +9,7 @@ import 'shared/settings_store.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) await BrowserContextMenu.disableContextMenu();
   runApp(
     PapatteParcApp(
       refugeStore: await ProgressStore.load(),
